0a7076da809f64884c585c7de55d5f4089854bb2
Rayimbek Jigitekov <96482537+Jigitekov@users.noreply.github.com>
2025-12-05 16:20:49 +0500

    Initial Commit

---
+++ Login_Registration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login_Registration
+//
+//  Created by Rayimbek Jigitekov on 05.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
